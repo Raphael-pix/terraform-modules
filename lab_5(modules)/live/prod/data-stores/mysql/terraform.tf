420486123e9384d298f1e3ce8c6d651f6b0560b8
Raphael-pix <113975698+Raphael-pix@users.noreply.github.com>
2026-03-10 12:28:54 +0300

    Updating folder structure

--- lab_5(modules)/live/prod/data-stores/mysql/terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-up-and-running-state-0952"
-    key    = "prod/data-stores/mysql/terraform.tfstate"
-    region = "us-east-1"
-
-    use_lockfile = true
-  }
-}
